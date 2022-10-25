5bf7e3a6937a255b607232765d891a2cca380cb4
LuisFX <luisfxx@hotmail.com>
2022-10-25 12:54:45 -0700

    Fix bug Wilken!!  😡

--- src/BusinessLayer/BusinessLayer.fs
+++ src/BusinessLayer/BusinessLayer.fs
@@ -23,34 +23,34 @@ module Logic =
     let updateCustomer capability =
         printfn "Enter new data: "
         match capability "customerData"  with
-        | Ok _ -> printfn "Data updated" 
+        | Ok _ -> printfn "Data updated"
         | Error err -> printfn ".. %A" err
 
     // use the updatePassword capability
     let updatePassword capability =
         printfn "Enter new password: "
         match capability "password"  with
-        | Ok _ -> printfn "Password updated" 
+        | Ok _ -> printfn "Password updated"
         | Error err -> printfn ".. %A" err
 module Capabilities =
 
-    let allCapabilities = 
-        
+    let allCapabilities =
+
         // apply the token, if present,
         // to a function which has only the token as a parameter
         let tokenToCap f token =
-            token 
-            |> Option.map (fun token -> 
+            token
+            |> Option.map (fun token ->
                 fun () -> f token)
 
         // apply the token, if present,
         // to a function which has the token and other parameters
         let tokenToCap2 f token =
-            token 
-            |> Option.map (fun token -> 
+            token
+            |> Option.map (fun token ->
                 fun x -> f token x)
 
-        let getCustomerOnlyForSameId id (principal:User)  = 
+        let getCustomerOnlyForSameId id (principal:User)  =
             let accessToken = Authorization.onlyForSameId id principal
             // MAXIME: The code below would fail to compile, because the AccessToken is of a different type, as expected.
             // match accessToken with
@@ -59,51 +59,51 @@ module Capabilities =
             // | None -> None
             accessToken |> tokenToCap CustomerDatastore.getCustomer
 
-        let getCustomerOnlyForAgentsInBusinessHours id (principal:User) = 
+        let getCustomerOnlyForAgentsInBusinessHours id (principal:User) =
             let accessToken = Authorization.onlyForAgents id principal
-            let cap1 = accessToken |> tokenToCap CustomerDatastore.getCustomer 
+            let cap1 = accessToken |> tokenToCap CustomerDatastore.getCustomer
             let restriction f = Authorization.onlyIfDuringBusinessHours (DateTime.Now) f
-            cap1 |> Authorization.restrict restriction 
+            cap1 |> Authorization.restrict restriction
 
-        let getCustomerOnlyForSameId_OrForAgentsInBusinessHours id (principal:User) = 
+        let getCustomerOnlyForSameId_OrForAgentsInBusinessHours id (principal:User) =
             async {
-                let cap1 = getCustomerOnlyForSameId id principal 
-                let cap2 = getCustomerOnlyForAgentsInBusinessHours id principal 
+                let cap1 = getCustomerOnlyForSameId id principal
+                let cap2 = getCustomerOnlyForAgentsInBusinessHours id principal
                 return Authorization.first [cap1; cap2]
             }
 
-        let updateCustomerOnlyForSameId id principal = 
+        let updateCustomerOnlyForSameId id principal =
             async {
                 let accessToken = Authorization.onlyForSameId id (principal:User)
                 return accessToken |> tokenToCap2 CustomerDatastore.updateCustomer
             }
 
-        let updateCustomerOnlyForAgentsInBusinessHours id principal = 
+        let updateCustomerOnlyForAgentsInBusinessHours id principal =
             async {
                 let accessToken = Authorization.onlyForAgents id principal
                 let cap1 = accessToken |> tokenToCap2 CustomerDatastore.updateCustomer
                 // uncomment to get the restriction
                 let restriction f = Authorization.onlyIfDuringBusinessHours (DateTime.Now) f // with restriction
                 // let restriction = Some  // no restriction
-                return cap1 |> Authorization.restrict restriction 
+                return cap1 |> Authorization.restrict restriction
             }
 
-        let updateCustomerOnlyForSameId_OrForAgentsInBusinessHours id (principal:User) = 
+        let updateCustomerOnlyForSameId_OrForAgentsInBusinessHours id (principal:User) =
             async {
-                let! cap1 = updateCustomerOnlyForSameId id principal 
-                let! cap2 = updateCustomerOnlyForAgentsInBusinessHours id principal 
+                let! cap1 = updateCustomerOnlyForSameId id principal
+                let! cap2 = updateCustomerOnlyForAgentsInBusinessHours id principal
                 return Authorization.first [cap1; cap2]
             }
 
-        let updatePasswordOnlyForSameId id (principal:User) = 
+        let updatePasswordOnlyForSameId id (principal:User) =
             async {
                 let accessToken = Authorization.passwordUpdate id principal
                 let cap = accessToken |> tokenToCap2 CustomerDatastore.updatePassword
-                return 
-                    cap 
+                return
+                    cap
                     |> Option.map (Authorization.auditable "UpdatePassword" principal.Name)
             }
-        
+
         let getTodosOnlyForUser principal : Async<option<('a -> Result<list<string>,FailureCase>)>> =
             async {
 
@@ -111,19 +111,19 @@ module Capabilities =
                 let cap = accessToken |> tokenToCap2 TodoDataStore.getTodos
                 return cap // |> Option.map (Authorization.auditable "GetTodos" principal.Name)
             }
-            
+
 
         // create the record that contains the capabilities
         {
-            getCustomer = getCustomerOnlyForSameId_OrForAgentsInBusinessHours 
-            updateCustomer = updateCustomerOnlyForSameId_OrForAgentsInBusinessHours 
+            getCustomer = getCustomerOnlyForSameId_OrForAgentsInBusinessHours
+            updateCustomer = updateCustomerOnlyForSameId_OrForAgentsInBusinessHours
             updatePassword = updatePasswordOnlyForSameId
-            getTodos = getTodosOnlyForUser 
+            getTodos = getTodosOnlyForUser //Get todos for user, for SAME CUSTOMER ID
         } : ICapabilityProvider
 
-    let getAllCapabilities customerId principal = 
-        let getCustomer = allCapabilities.getCustomer customerId principal 
-        let updateCustomer = allCapabilities.updateCustomer customerId principal 
+    let getAllCapabilities customerId principal =
+        let getCustomer = allCapabilities.getCustomer customerId principal
+        let updateCustomer = allCapabilities.updateCustomer customerId principal
         let updatePassword = allCapabilities.updatePassword customerId principal
         let getTodos = allCapabilities.getTodos principal
         getCustomer, updateCustomer , updatePassword, getTodos
